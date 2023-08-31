1e7f3ece04ac4aa57ef6bd19a003964e3413ed0a
tlc2880 <tommylcao@gmail.com>
2023-08-31 05:19:13 -0700

    Add backend user auth and registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,18 +1,56 @@
 import asyncHandler from 'express-async-handler'
-import user from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js'
+import User from '../models/userModel.js';
 
 // @desc    Auth user/set token
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User' });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id )
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password')
+    }
+    
 });
 
 // @desc    Register a new user
 // route    POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Register User' });
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
 });
 
 // @desc    Logout user

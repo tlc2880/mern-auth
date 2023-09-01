a3d96f62c7531d58ca8851c42e84b5081fbff570
tlc2880 <tommylcao@gmail.com>
2023-09-01 04:58:40 -0700

    Backend auth user profile update

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 
-dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 

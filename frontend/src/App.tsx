ee3294d212927e98f575b644c9bbe10d9717798f
tlc2880 <tommylcao@gmail.com>
2023-09-03 07:34:49 -0700

    Add login and register screens and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Header from './components/Header';
+import { Container } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <Header/>
-      <Outlet />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   );
 };

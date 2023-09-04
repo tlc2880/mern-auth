1c736027016626497b6e01718399e6c33867fe24
tlc2880 <tommylcao@gmail.com>
2023-09-04 04:52:21 -0700

    Add react redux toolkit auth slices

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,16 +6,16 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-            <Navbar.Brand href='/'>MERN Auth TypeScript</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Brand href='/'>MERN Auth TypeScript</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-                <Nav.Link href='/login'>
-                    <FaSignInAlt /> Sign In
-                </Nav.Link>
-                <Nav.Link href='/register'>
-                    <FaSignOutAlt /> Sign Up
-                </Nav.Link>
+              <Nav.Link href='/login'>
+                <FaSignInAlt /> Sign In
+              </Nav.Link>
+              <Nav.Link href='/register'>
+                <FaSignOutAlt /> Sign Up
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>

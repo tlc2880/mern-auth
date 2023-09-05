961bb8d732e92992dead9c23e1c0d8871ed760bc
tlc2880 <tommylcao@gmail.com>
2023-09-05 06:27:24 -0700

    Frontend login toastify auth header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <>
-      <Header/>
-      <Outlet />
+      <Header />
+      <ToastContainer />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   );
 };
 
-export default App
+export default App;
--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,17 +1,41 @@
-import { useState, FormEvent  } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect, FormEvent } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import { useLoginMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
 export default function LoginScreen() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [login, { isLoading }] = useLoginMutation();
+
+    const { userInfo } = useSelector((state: any) => state.auth);
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate('/');
+        }
+    }, [navigate, userInfo]);
+
     const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submit');
-    }
-
+        try {
+            const res = await login({ email, password }).unwrap();
+            dispatch(setCredentials({...res}))
+            navigate('/')
+        } catch(err: any) {
+            toast.error(err?.data?.message || err.error);
+        }
+    };
+    
   return (
     <FormContainer>
         <h1>Sign In</h1>
@@ -38,20 +62,23 @@ export default function LoginScreen() {
             </Form.Group>
 
             <Button
-                
+                disabled={isLoading}
                 type='submit'
                 variant='primary'
                 className='mt-3'
             >
                 Sign In
             </Button>
-
-            <Row className='py-3'>
-                <Col>
-                    New Customer? <Link to='/register'>Register</Link>
-                </Col>
-            </Row>
         </Form>
+
+        {isLoading && <Loader />}
+
+        <Row className='py-3'>
+            <Col>
+                New Customer? <Link to='/register'>Register</Link>
+            </Col>
+        </Row>
+
     </FormContainer>
   );
 }
